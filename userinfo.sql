854adcb6cbd77a32f294747858a55847a426939f
nthhaf <captainnemot1k60@gmail.com>
2023-05-19 18:33:27 +0700

    create userinfo database

---
+++ userinfo.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE userinfo;
+
+USE userinfo;
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+INSERT INTO users (username, password)
+VALUES ('0987654321', 'q2ư2e3'),
+       ('0987452100', 'so1dcv'),
+       ('0983313567', 'ki98u7'),
+       ('0912345678', 'ngaythu5');
